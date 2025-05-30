95d4854a85b1f8b4a017734df5d905f5cd9cd887
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2025-05-30 15:53:07 +0300

    dev: tailwind and project theme

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      white: "#fff",
+      grey: {
+        300: "#666",
+        400: "#4a4d4e",
+        500: "#3C3F40",
+        600: "#333",
+        650: "#303233"
+      },
+      red: "#e60000"
+    },
+    extend: {
+      fontFamily: {
+        regular: ["Vodafone Rg", "sans"],
+        light: ["Vodafone Lt", "sans"]
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
