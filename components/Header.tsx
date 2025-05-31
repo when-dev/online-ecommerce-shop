e6deb1522021fa9f4d9b450b81c03a2fecbda653
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2025-05-31 14:37:09 +0300

    fix: change header structure

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,72 +31,86 @@ const Cart: FC<{ items?: number }> = ({ items }) => {
   );
 };
 
-const Header: FC = () => {
+const MobileHeader: FC = () => {
   return (
-    <header className="flex items-center bg-white border-b border-grey-100 lg:border-b-0">
-      <div className="relative flex items-center justify-between h-14 px-4 w-full lg:hidden">
-        <button className="w-6 h-6 z-10">
-          <BurgerIcon />
-        </button>
-        <div className="absolute left-1/2 transform -translate-x-1/2 w-16 z-0">
-          <Link href="/">
-            <Logo />
-          </Link>
-        </div>
-        <button className="w-5 z-10">
-          <SearchIcon />
-        </button>
+    <div className="relative flex items-center justify-between h-14 px-4 w-full lg:hidden">
+      <button className="w-6 h-6 z-10">
+        <BurgerIcon />
+      </button>
+      <div className="absolute left-1/2 transform -translate-x-1/2 w-16 z-0">
+        <Link href="/">
+          <Logo />
+        </Link>
       </div>
-      <div className="hidden lg:flex flex-col w-full">
-        <div className="bg-grey-50 text-grey-500 w-full border-b border-grey-100">
-          <div className="flex justify-between items-center h-12 max-w-7xl mx-auto text-sm px-4">
-            <div className="flex items-center space-x-6">
-              {staticLinks.map(({ name, link }) => (
-                <Link href={link} key={name} className="hover:underline">
-                  {name}
-                </Link>
-              ))}
-            </div>
-            <div className="flex items-center space-x-6">
-              <a href="tel:+79287775680" className="hover:underline">
-                8-(928)-777-56-80
-              </a>
-              <button className="flex items-center hover:underline">
-                <GeoIcon className="w-3 h-3 mr-1" />
-                Краснодар
-              </button>
-              <div className="relative group flex-shrink-0">
-                <button className="flex items-center hover:underline">
-                  Рус
-                  <ArrowIcon className="w-2 h-2 ml-1 transform group-hover:-rotate-90 transition-transform" />
-                </button>
+      <button className="w-5 z-10">
+        <SearchIcon />
+      </button>
+    </div>
+  );
+};
 
-                <div className="absolute right-0 top-full flex-col bg-white border border-grey-200 px-3 py-2 rounded shadow-md hidden group-hover:flex z-10 text-sm min-w-[60px] whitespace-nowrap">
-                  <button className="hover:text-orange">Eng</button>
-                </div>
+const DesktopHeader: FC = () => {
+  return (
+    <div className="hidden lg:flex flex-col w-full">
+      <div className="bg-grey-50 text-grey-500 w-full border-b border-grey-100">
+        <div className="flex justify-between items-center h-12 max-w-7xl mx-auto text-sm px-4">
+          <div className="flex items-center space-x-6">
+            {staticLinks.map(({ name, link }) => (
+              <Link href={link} key={name} className="hover:underline">
+                {name}
+              </Link>
+            ))}
+          </div>
+          <div className="flex items-center space-x-6">
+            <a href="tel:+79287775680" className="hover:underline">
+              8-(928)-777-56-80
+            </a>
+            <button className="flex items-center hover:underline">
+              <GeoIcon className="w-3 h-3 mr-1" />
+              Краснодар
+            </button>
+            <div className="relative group flex-shrink-0">
+              <button className="flex items-center hover:underline">
+                Рус
+                <ArrowIcon className="w-2 h-2 ml-1 transform group-hover:-rotate-90 transition-transform" />
+              </button>
+
+              <div className="absolute right-0 top-full flex-col bg-white border border-grey-200 px-3 py-2 rounded shadow-md hidden group-hover:flex z-10 text-sm min-w-[60px] whitespace-nowrap">
+                <button className="hover:text-orange">Eng</button>
               </div>
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="bg-white h-16 shadow-sm">
-          <div className="flex justify-between items-center h-full max-w-7xl mx-auto px-4">
-            <div className="flex items-center space-x-6 min-w-max">
-              <Link href="/" className="w-20 text-red">
-                <Logo />
-              </Link>
-              <button className="flex items-center text-black text-lg font-regular hover:text-grey-600 transition-colors hover:">
-                <MenuIcon className="w-5 h-5 mr-2" />
-                Каталог товаров
-              </button>
-            </div>
-            <div className="flex-1 mx-6">
-              <Input placeholder="Поиск по товарам" type="text" error={false} />
-            </div>
-            <Cart items={3} />
+      <div className="bg-white h-16 shadow-sm">
+        <div className="flex justify-between items-center h-full max-w-7xl mx-auto px-4">
+          <div className="flex items-center space-x-6 min-w-max">
+            <Link href="/" className="w-20 text-red">
+              <Logo />
+            </Link>
+            <button className="flex items-center text-black text-lg font-regular hover:text-grey-600 transition-colors hover:">
+              <MenuIcon className="w-5 h-5 mr-2" />
+              Каталог товаров
+            </button>
           </div>
+          <div className="flex-1 mx-6">
+            <Input placeholder="Поиск по товарам" type="text" error={false} />
+          </div>
+          <Link href="/">
+            <Cart items={3} />
+          </Link>
         </div>
       </div>
+    </div>
+  );
+};
+
+const Header: FC = () => {
+  return (
+    <header className="flex items-center bg-white border-b border-grey-100 lg:border-b-0">
+      <MobileHeader />
+      <DesktopHeader />
     </header>
   );
 };
