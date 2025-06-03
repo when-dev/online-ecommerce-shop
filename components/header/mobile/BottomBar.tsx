7f2815e317e665548ad5af0f7d2219cd9ab4898b
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2025-06-03 18:36:16 +0300

    fix: replace bottom bar line with shadow

--- components/header/mobile/BottomBar.tsx
+++ components/header/mobile/BottomBar.tsx
@@ -18,11 +18,11 @@ const navItems = [
 ];
 
 const BottomBar = () => {
-	// TODO: add router for active link highlighting
+  // TODO: add router for active link highlighting
   // const router = useRouter();
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t border-grey-200 flex justify-between items-center px-2 py-1 lg:hidden">
+    <nav className="fixed bottom-0 left-0 right-0 z-50 bg-white border-grey-200 flex justify-between items-center px-2 py-2 mb-1 lg:hidden shadow-[0_-5px_5px_-5px_rgba(0,0,0,0.1)]">
       {navItems.map(item => (
         <Link
           key={item.label}
