8f0d72fabb2b40931f8619a234de0d6a1121b432
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2025-06-05 12:14:01 +0300

    feat: added submenu slice for mobile menu

---
+++ redux/slices/submenu/submenuSlices.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface SubmenuState {
+	isOpen: boolean;
+	category: string | null;
+}
+
+const initialState: SubmenuState = {
+	isOpen: false,
+	category: null,
+}
+
+const submenuSlice = createSlice({
+	name: "submenu",
+	initialState,
+	reducers: {
+		openSubmenu: (state, action: PayloadAction<string>) => {
+			state.isOpen = true;
+			state.category = action.payload;
+		},
+		closeSubmenu: (state) => {
+			state.isOpen = false;
+			state.category = null;
+		},
+	}
+})
